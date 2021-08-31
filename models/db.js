e2eb2c432853841e2a91cf797265a39d8e6153bb
Hassaan <hassaanch1111@gmail.com>
2021-08-31 14:15:05 +0500

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://{process.env.server}:{process.env.PORT}`/EmployeeDB`, { useNewUrlParser: true }, (err) => {
+mongoose.connect(`mongodb://{process.env.SERVER}:{process.env.PORT}/{process.env.DB}`, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
